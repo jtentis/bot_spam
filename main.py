f8b6ce604bd7a23613145e8d4e9bbde9fc14a77e
Joao Tentis <jps.tentis@gmail.com>
2023-05-13 21:50:04 -0300

    teste

---
+++ main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests 
+import pyautogui
+import time
+
+# Essa é a função de spam, que tem 2 parametros: url e tag.
+def bot_spam(url, tag):
+    time.sleep(5)
+    req = requests.get(url)
+    parse = BeautifulSoup(req.text, 'html.parser')
+    html = parse.find_all(tag) #pegar no site tudo que tem a tag selecionada
+    print(html)
+
+    for mensagem in html:
+        pyautogui.typewrite(mensagem.get_text())
+        pyautogui.press("enter")
+        time.sleep(1)
+
+bot_spam('https://www.mensagens10.com.br/mensagens-de-bom-dia', 'p')
+    
